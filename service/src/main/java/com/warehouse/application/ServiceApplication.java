7a371dc1b52fb09d47614a4727d3b6db44ac19e0
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2026-07-01 20:24:46 +0700

    chore : add Annotation for Repository Packages

--- service/src/main/java/com/warehouse/application/ServiceApplication.java
+++ service/src/main/java/com/warehouse/application/ServiceApplication.java
@@ -3,9 +3,11 @@ package com.warehouse.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.warehouse")
 @EntityScan(basePackages = "com.warehouse")
+@EnableJpaRepositories(basePackages = "com.warehouse.repository.repo")
 public class ServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
