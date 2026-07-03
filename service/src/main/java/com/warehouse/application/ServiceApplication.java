bada3f1ea7206a3c288edec860af97c5e95726bb
Thanthiti <145197437+Thanthiti@users.noreply.github.com>
2026-07-03 16:06:04 +0700

    chore: change package name

--- service/src/main/java/com/warehouse/application/ServiceApplication.java
+++ service/src/main/java/com/warehouse/application/ServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.warehouse")
 @EntityScan(basePackages = "com.warehouse")
-@EnableJpaRepositories(basePackages = "com.warehouse.repository.repo")
+@EnableJpaRepositories(basePackages = "com.warehouse.repository")
 public class ServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
